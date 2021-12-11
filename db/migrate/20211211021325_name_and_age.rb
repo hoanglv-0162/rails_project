2819525519088c48d7728d408a3bde0b70ffe0c2
hoanglv-0162 <hoangh2kq@gmail.com>
2021-12-11 02:54:32 +0000

    add name&age

---
+++ db/migrate/20211211021325_name_and_age.rb
@@ -0,0 +1,8 @@
+class NameAndAge < ActiveRecord::Migration[6.0]
+  def change
+      add_column :users, :name, :string
+      add_column :users, :age, :integer
+      
+      add_index :users, :name, unique: true
+  end
+end
