a9555c746fed5706748df06a39ddb1373a057d4a
bao <baogamemail@gmail.com>
2021-12-11 12:38:05 +0000

    update play song

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,4 @@
 class Song < ApplicationRecord
-    has_attached_file :mp3
-    validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
+    has_one_attached :mp3
     
 end
